44baec147d798eb2a900c47d71994e36a01eeed5
Stefanos Bekiaris <stefanosbek@hotmail.com>
2020-01-12 16:34:55 +1100

    update

--- arduino_drone_logic.py
+++ arduino_drone_logic.py
@@ -4,11 +4,6 @@ import time
 
 import serial
  
-port = serial.Serial('/dev/ttyUSB0', 9600) # change name, if needed
-port.close()
-port.open()
-time.sleep(5) # the Arduino is reset after enabling the serial connection, therefore we have to wait some seconds
-
 def main():
     # set up
     #serial set up
