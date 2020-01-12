ca8c62262a4caa664693f7b95574d6023cfe71fc
Stefanos Bekiaris <stefanosbek@hotmail.com>
2020-01-12 15:11:54 +1100

    update

--- arduino_drone_logic.py
+++ arduino_drone_logic.py
@@ -5,6 +5,7 @@ import gyro
 import serial
  
 s = serial.Serial('/dev/ttyAMA0', 9600) # change name, if needed
+s.close()
 s.open()
 time.sleep(5) # the Arduino is reset after enabling the serial connection, therefore we have to wait some seconds
  
