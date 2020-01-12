0537e7228b2f8d114783bf6ff0de51d16b1f60c6
Stefanos Bekiaris <stefanosbek@hotmail.com>
2020-01-12 16:40:52 +1100

    update

--- serialTest.py
+++ serialTest.py
@@ -2,6 +2,7 @@ import serial
 import time
  
 s = serial.Serial('/dev/ttyUSB2', 9600) # change name, if needed
+s.close()
 s.open()
 time.sleep(5) # the Arduino is reset after enabling the serial connectio, therefore we have to wait some seconds
  
