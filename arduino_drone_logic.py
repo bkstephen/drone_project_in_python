6595752113afaf14b6a17662562798795217cfe6
Stefanos <sbekiaris@switchautomation.com>
2020-04-02 22:26:33 +1100

    finilized microcontroler

--- arduino_drone_logic.py
+++ arduino_drone_logic.py
@@ -2,33 +2,15 @@ import time
 #import gyro
 
 import serial
+import serial_communication
  
 def main():    
-    #serial set up
-    port = serial.Serial('/dev/ttyUSB0', 9600) # change name, if needed
-    port.close()
-    port.open()
-    time.sleep(5)
 
-    #the motor has 5 speeds (5, 6, 7, 8, 9)
-    try:
-        while True:
-            s = input("Give thrust: ")
-            p = input("Propeller (A/x/X/y/Y)")            
-             
-            # Send the code to be decoded p if the propeller, s is the speed, e determines the end of the data              
-            port.write(str.encode(p))        
-            port.write(str.encode(s))                  
-            # port.write(str.encode("e"))             
-
-    except KeyboardInterrupt:
-        port.close()        
-
-try:
-    main()  #run main()
-except:
-    print("Something went wrong")
-finally:
-    main()
+    while True:
+        s = input("Give thrust: ")
+        p = input("Propeller (A/x/X/y/Y)")     
+        serial_communication.write(s, p)       
+    
+main()
 
 #TODO create communicaiton between the arduino and pi using serial9600 by decoding the data received
--- serial_communication.py
+++ serial_communication.py
@@ -1,32 +1,23 @@
 import time
 import serial
  
-def main():       
-
-    while True:
-        #the motor has 5 speeds (5, 6, 7, 8, 9)
-        s = input("Give thrust: ")
-        p = input("Propeller (A/x/X/y/Y)")            
-        send_data(s, p, '/dev/ttyUSB0');
-        # Send the code to be decoded p if the propeller, s is the speed, e determines the end of the data              
-                           
-  
-
-def send_data(s , p, serialLocationString):
+def write(s, p):       
     try:
         #serial set up
-        port = serial.Serial(serialLocationString, 9600) # change USB, if needed
+        port = serial.Serial('/dev/ttyUSB0', 9600) # change USB, if needed
         port.close()
         port.open()        
-        time.sleep(5)
+        time.sleep(1)
+
+        while True:
+            #the motor has 5 speeds (5, 6, 7, 8, 9)         
+            send_data(s, p, port);
+            # Send the code to be decoded p if the propeller, s is the speed, e determines the end of the data 
+         
+    except KeyboardInterrupt:        
+        port.close()        
+    
+def send_data(s , p, port):
         port.write(str.encode(p))        
         port.write(str.encode(s))  
-    except KeyboardInterrupt:        
-        port.close()   
-
-try:
-    main()  #run main()
-except:
-    print("Something went wrong")
-finally:
-    main()
+    
